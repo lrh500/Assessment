8f73c1e94c395291e10cf92868547c88a56ba272
rz871256465 <871256465@qq.com>
2023-03-21 20:12:02 +0000

    test

--- assessment/management/commands/parse_csv.py
+++ assessment/management/commands/parse_csv.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from django.db import models
 from django.core.management.base import BaseCommand, CommandError
 
-from country_id.models import country_id
+from country_id.models import Country_id
 
 class Command(BaseCommand):
 help = 'Load data from csv'
@@ -12,26 +12,21 @@ help = 'Load data from csv'
 def handle(self, *args, **options):
 
     # drop the data from the table so that if we rerun the file, we don't repeat values
-    Bear.objects.all().delete()
+    Country_id.objects.all().delete()
     print("table dropped successfully")
     # create table again
 
     # open the file to read it into the database
     base_dir = Path(__file__).resolve().parent.parent.parent.parent
-    with open(str(base_dir) + '/bears/PolarBear_Telemetry_southernBeaufortSea_2009_2011/USGS_WC_eartag_deployments_2009-2011.csv', newline='') as f:
+    with open(str(base_dir) + '/assessment/database/Pcountry and id .csv', newline='') as f:
         reader = csv.reader(f, delimiter=",")
         next(reader) # skip the header line
         for row in reader:
             print(row)
 
-            bear = Bear.objects.create(
-            bearID = int(row[0]),
-            pTT_ID = int(row[1]),
-            capture_lat = float(row[6]),
-            capture_long = float(row[7]),
-            sex = row[9],
-            age_class = row[10],
-            ear_applied = row[11],
+            country_id = Country_id.objects.create(
+            id = int(row[0]),
+            Country = row[1],
             )
-            bear.save()
+            country_id.save()
     print("data parsed successfully")
