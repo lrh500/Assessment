0c454a7227fbfc654c355f881f562998c48eb92e
QINGYANG ZENG <q.zeng.22@abdn.ac.uk>
2023-03-23 13:42:10 +0000

    change

--- assessment/climate_change/management/commands/parse_csv.py
+++ assessment/climate_change/management/commands/parse_csv.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from django.db import models
 from django.core.management.base import BaseCommand, CommandError
 
-from climate_change.models import country_id
+from climate_change.models import country_id,Global_tem
 
 class Command(BaseCommand):
     help = 'Load data from csv'
@@ -26,8 +26,63 @@ class Command(BaseCommand):
 
                 country = country_id.objects.create(
                     id=int(row[0]),
-                    Country=row[1],
+                    country=row[1],
                 )
                 country.save()
 
-        print("data parsed successfully")
+            #else:
+                #next(reader)
+
+        
+        Global_tem.objects.all().delete()
+        print("table dropped successfully")
+
+        with open(str(base_dir) + '/climate_change/database/GlobalLandTemperaturesByCity.csv', newline='') as f:        
+            reader = csv.reader(f, delimiter=",")
+            next(reader) # skip the header line
+            for row in reader:
+                print(row)   
+
+                
+                if row[5] is  not '':
+                    country_temp = row[0]  
+                    print(country_temp)
+                    global_tem = Global_tem.objects.filter(country_id = country_temp).first()
+                    print(country.id)
+
+                global_tem = Global_tem.objects.create(
+                    id = int(row[0]),
+                    country = country_id,
+                    dt=row[1],
+                    averageTemperature=row[2],
+                    averageTemperatureUncertainty=row[3],
+                    city=row[4],
+                    country_id = country,
+                    latitude=row[6],
+                    longitude=row[7]
+                    )
+
+                    # Save the Global_tem object to the database
+                global_tem.save()
+            
+            else:
+                next(reader)
+
+
+            if row[4] is  not '':
+                bear_temp = row[0]  
+                print(bear_temp)
+                bear = Bear.objects.filter(bearID = bear_temp).first()
+                print(bear.id)
+
+                sighting = Sighting.objects.create(
+                deploy_id = int(row[0]),
+                bear_id = bear,
+                recieved = row[2],
+                latitude = float(row[4]),
+                longitude = float(row[5]),
+                temperature = float(row[9]),
+                )
+                sighting.save()
+
+print("data parsed successfully")
--- assessment/climate_change/models.py
+++ assessment/climate_change/models.py
@@ -3,8 +3,8 @@ from django.conf import settings
 # Create your models here.
 
 class country_id(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    Country = models.TextField()
+    id = models.IntegerField()
+    country = models.TextField(primary_key=True)
 
     def __str__(self):
             return f'{self.id}, {self.Country}'
@@ -12,17 +12,16 @@ class country_id(models.Model):
 
 class Global_tem(models.Model):
     id = models.BigAutoField(primary_key=True)
-    Country = models.ForeignKey('climate_change.country_id',on_delete=models.CASCADE, related_name='Global_tem')
+    country = models.ForeignKey('climate_change.country_id',on_delete=models.CASCADE, related_name='Global_tem')
     dt = models.TextField()
-    AverageTemperature = models.TextField()
-    AverageTemperatureUncertainty = models.TextField()
-    City = models.TextField()
-    Latitude = models.TextField()
-    Longitude = models.TextField()
+    averageTemperature = models.TextField()
+    averageTemperatureUncertainty = models.TextField()
+    city = models.TextField()
+    latitude = models.TextField()
+    longitude = models.TextField()
 
     def __str__(self):
-            return f'{self.id}, {self.Country}, {self.dt},{self.AverageTemperature},{self.AverageTemperatureUncertainty},{self.City},{self.Latitude},{self.Longitude}'
-
+            return f'{self.id}, {self.country}, {self.dt},{self.averageTemperature},{self.averageTemperatureUncertainty},{self.city},{self.latitude},{self.longitude}'
 
 
 
