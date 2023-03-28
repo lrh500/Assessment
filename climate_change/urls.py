91db51d69256b29e3bbff401953b6bed25162784
rz871256465 <871256465@qq.com>
2023-03-28 17:07:25 +0100

    change link about all html

--- climate_change/urls.py
+++ climate_change/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('country_list', views.country_list, name='country_list'),
+    path('country_list/', views.country_list, name='country_list'),
     path('', views.index, name='index'),
     path('global_tem_list/', views.global_tem_list, name='global_tem_list'),
     path('global_tem_changes/', views.global_tem_change_view, name='global_tem_changes'),
