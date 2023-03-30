78632ac96f4fb7568e5ceb3e57fb1067689dba05
RUIHAO LIU <r.liu.22@abdn.ac.uk>
2023-03-30 13:30:56 +0000

    change the static root

--- assessment/settings.py
+++ assessment/settings.py
@@ -121,6 +121,12 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'climate_change/templates/climate_change/img'),
